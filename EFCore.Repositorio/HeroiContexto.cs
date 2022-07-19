819daf78c206bccb5d5a43d808fd40b82bfdca9b
ThiagoPaiva89 <thiagoinaciopaiva@hotmail.com>
2022-07-19 10:24:22 -0300

    Inserindo métodos Http

--- EFCore.Repositorio/HeroiContexto.cs
+++ EFCore.Repositorio/HeroiContexto.cs
@@ -1,25 +1,26 @@
-﻿using EFCore.WebAPI.Models;
+﻿using EFCore.Dominio;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFCore.WebAPI.Data
+namespace EFCore.Repositorio
 {
     public class HeroiContexto : DbContext
     {
+
+        public HeroiContexto(DbContextOptions<HeroiContexto> options) : base(options)
+        {
+
+        }
+
         public DbSet<Heroi> Herois { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
         public DbSet<Arma> Armas { get; set; }
         public DbSet<HeroiBatalha> HeroiBatalhas { get; set; }
         public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
 
-        //Método para string de conexão
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-QJS44MF\\SQLEXPRESS;Initial Catalog=HeroApp;Integrated Security=True;User Id=sa; Password=395218hyn");
-        }
 
         //Define as chaves compostas para uma tabela de relacionamento
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ EFCore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,87 @@
+﻿using EFCore.Dominio;
+using EFCore.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCore.WebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        public readonly HeroiContexto _contexto;
+
+        public ValuesController(HeroiContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet("filtro/{nome}")]
+        public ActionResult GetFiltro(string nome)
+        {
+            //var listHeroi = _contexto.Herois.ToList();
+            var listaHeroi = (from heroi in _contexto.Herois
+                              where heroi.Nome.Contains(nome)
+                              select heroi).ToList();
+            return Ok(listaHeroi);
+        }
+
+        //// GET api/<ValuesController>/5
+        //[HttpGet("{nomeHeroi}")]
+        //public ActionResult Get(string nomeHeroi)
+        //{
+        //    var heroi = new Heroi { Nome = nomeHeroi };
+        //    _contexto.Herois.Add(heroi);
+        //    _contexto.SaveChanges();
+        //    return Ok();
+        //}
+
+        // GET api/<ValuesController>/5
+        //[HttpGet("{AddRange}")]
+        //public ActionResult GetAddRange()
+        //{
+        //    _contexto.AddRange(
+        //        new Heroi { Nome = "Capitão América"},
+        //        new Heroi { Nome = "Doutor Estranho" },
+        //        new Heroi { Nome = "Pantera Negra" },
+        //        new Heroi { Nome = "Viúva Negra" },
+        //        new Heroi { Nome = "Hulk" },
+        //        new Heroi { Nome = "Gavião Arqueiro" },
+        //        new Heroi { Nome = "Capitão Marvel" }
+        //        );
+        //    _contexto.SaveChanges();
+        //    return Ok();
+        //}
+
+
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpGet("Delete/{id}")]
+        public void Delete(int id)
+        {
+            var heroi = _contexto.Herois.Where(x => x.Id == id).Single();
+            _contexto.Herois.Remove(heroi);
+            _contexto.SaveChanges();
+        }
+    }
+}
